992954ec423717de04fd43e86236b5d085a83365
mdeandwibekti <mdeandwibekti@gmail.com>
2025-11-21 16:36:05 +0700

    membuat fun dengan menambahkan siswaviewmodel

---
+++ app/src/main/java/com/example/quest6_042/view/uicontroller/Navigasi.kt
@@ -0,0 +1,30 @@
+package com.example.quest6_042.view.uicontroller
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.quest6_042.model.DataJK.JenisK
+import com.example.quest6_042.view.FormIsian
+import com.example.quest6_042.view.TampilanData
+import com.example.quest6_042.viewmodel.SiswaViewModel
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    modifier: Modifier,
+    viewModel: SiswaViewModel = viewModel (),
+    navController: NavHostController = rememberNavController()
+) {
+}
