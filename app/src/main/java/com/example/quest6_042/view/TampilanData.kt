6905073442503619c5e08e1754d94a6e41135c86
mdeandwibekti <mdeandwibekti@gmail.com>
2025-11-21 16:51:48 +0700

    membuat topbar untuk form isian data dengan mengset warna

---
+++ app/src/main/java/com/example/quest6_042/view/TampilanData.kt
@@ -0,0 +1,48 @@
+package com.example.quest6_042.view
+
+import android.graphics.fonts.FontFamily
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.quest6_042.R
+import com.example.quest6_042.model.Siswa
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilanData(
+    statusUiSiswa: Siswa,
+    onBackBtnClick: () -> Unit
+) {
+
+    val items = listOf("Nama Lengkap" to statusUiSiswa.nama, "Jenis Kelamin" to statusUiSiswa.gender, "Alamat" to statusUiSiswa.alamat)
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(id = R.string.tampil), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    ) {
+    }
+}
